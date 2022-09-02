c2a92df9b44de5c575476426a418454ac221de53
Austin Szatrowski <austinszatrowski@gmail.com>
2022-09-02 11:36:56 -0400

    added more complete example

--- R/mr_analyze.R
+++ R/mr_analyze.R
@@ -9,6 +9,27 @@
 #'
 #' @examples
 #' mr_analyze("prot-a-16", "finn-b-I9_CARDMYO", 1e-06)
+#' Extracting data for 4 SNP(s) from 1 GWAS(s)
+#' Harmonising  || id:prot-a-16 (prot-a-16) and Cardiomyopathy || id:finn-b-I9_CARDMYO 
+#' (finn-b-I9_CARDMYO)
+#'                     Method Estimate Std Error 95% CI         P-value
+#'              Simple median    0.006     0.139  -0.268  0.279   0.968
+#'            Weighted median   -0.013     0.139  -0.286  0.261   0.928
+#'  Penalized weighted median   -0.013     0.139  -0.286  0.261   0.928
+#'                                                                     
+#'                        IVW   -0.096     0.113  -0.318  0.125   0.394
+#'              Penalized IVW   -0.096     0.113  -0.318  0.125   0.394
+#'                 Robust IVW   -0.043     0.338  -0.704  0.619   0.900
+#'       Penalized robust IVW   -0.043     0.338  -0.704  0.619   0.900
+#'                                                                     
+#'                   MR-Egger    0.627     0.470  -0.294  1.547   0.182
+#'                (intercept)   -0.229     0.145  -0.512  0.054   0.113
+#'         Penalized MR-Egger    0.627     0.470  -0.294  1.547   0.182
+#'                (intercept)   -0.229     0.145  -0.512  0.054   0.113
+#'            Robust MR-Egger    0.628     0.158   0.319  0.937   0.000
+#'                (intercept)   -0.230     0.045  -0.318 -0.141   0.000
+#'  Penalized robust MR-Egger    0.628     0.158   0.319  0.937   0.000
+
 mr_analyze <- function(exposure, outcome, p) {
     # extract exposures and outcomes from MRCIEU openGWAS
     exposures <- TwoSampleMR::extract_instruments(exposure, p1 = p) # nolint
@@ -44,5 +65,4 @@ mr_analyze <- function(exposure, outcome, p) {
 
     mr_all_result <- MendelianRandomization::mr_allmethods(input)
     print(mr_all_result)
-
 }
